ff127ca0a4248b11f56c884c4882c5a3310991e1
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-02-12 19:14:43 -0500

    first commit

---
+++ baby_steps.js
@@ -0,0 +1,11 @@
+// console.log(process.argv);
+
+var sum = 0;
+var y;
+for(var x = 2; x < process.length;x++){
+y = Number(process.argv[x]);
+  return sum = sum + y;
+  console.log(sum);
+}
+
+console.log(sum);
